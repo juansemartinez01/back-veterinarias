083f98b21a34b8fc600578f70949567f6de5d77d
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-09-09 10:29:39 -0300

    base

--- src/servicios/servicios.module.ts
+++ src/servicios/servicios.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Servicio } from './servicio.entity';
 import { ServiciosService } from './servicios.service';
 import { ServiciosController } from './servicios.controller';
+import { RbacModule } from 'src/auth/rbac/rbac.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Servicio])],
+  imports: [TypeOrmModule.forFeature([Servicio]),RbacModule],
   providers: [ServiciosService],
   controllers: [ServiciosController],
   exports: [ServiciosService],
