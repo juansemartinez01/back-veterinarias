21a24d4b22131baf5ababead541ab7cf6cbbfbc6
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-09-26 11:22:02 -0300

    Modificaciones en paciente

--- src/pacientes/pacientes.module.ts
+++ src/pacientes/pacientes.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Paciente } from './paciente.entity';
 import { PacientesService } from './pacientes.service';
 import { PacientesController } from './pacientes.controller';
-import { Cliente } from 'src/clientes/cliente.entity';
-import { Especie } from 'src/especies/especie.entity';
-import { Raza } from 'src/razas/raza.entity';
+import { Cliente } from '../clientes/cliente.entity';
+import { Especie } from '../especies/especie.entity';
+import { Raza } from '../razas/raza.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Paciente, Cliente, Especie, Raza])],
